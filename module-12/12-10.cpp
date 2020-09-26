267c5f9e28f95fffb1cf8177d94a021a5470493c
Srinivasulu Charupally <SrinivasuluCharupally@github.com>
2020-09-27 03:07:47 +0530

    fixing compilation error

--- module-12/12-10.cpp
+++ module-12/12-10.cpp
@@ -1,15 +1,15 @@
-// Malloc: Write an aligned malloc and free function that supports allocating memory such that the memory address returned is divisible by a speci c power of two.EXAMPLEalign_malloc(1000, 128) will return a memory address that is a multiple of 128 and that points to memory of size 1000 byte
-
-
-
+/*
+// Malloc: Write an aligned malloc and free function that supports allocating memory such that the memory address returned is divisible by a speci c power of two.
+// EXAMPLEalign_malloc(1000, 128) will return a memory address that is a multiple of 128 and that points to memory of size 1000 byte
 // source https://sites.google.com/site/ruslancray/lab/bookshelf/interview/ci/low-level/write-an-aligned-malloc-free-function
-
 // source for class type: http://www.cnblogs.com/grandyang/p/4932046.html
+*/
 
 
 #include<iostream>
 #include<stdlib.h>
 #include<stdio.h>
+
 using namespace std;
 
 void* aligned_malloc(size_t required_bytes, size_t alignment) {
@@ -19,7 +19,8 @@ void* aligned_malloc(size_t required_bytes, size_t alignment) {
 	p1 = (void*)malloc(required_bytes + offset);
 	if(p1 == NULL) return NULL; // no memory available
 
-	p2 = (void**)(((size_t)(p1) + offset) & ~(alignment -1));
+	p2 = (void**)(((size_t)(p1) + offset) & ~(alignment -1)); // size_t(p1) will give you starting address of p1
+	cout << "size of p1 " << size_t(p1) << endl;
 	p2[-1] = p1;
 	return p2;
 }
